69a96e05ba8ea61fb29bb7c5b67c9f9c902c28c2
Krishanna Williams <krishannaw@gmail.com>
2023-04-05 23:40:03 -0500

    arrow

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Arrow function called add
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
